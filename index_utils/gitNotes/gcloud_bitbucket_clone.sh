657262fc5a926832a89be918acab113223453129
Paul Desmond <paul.desmond@skytra.com>
2020-03-25 09:44:55 +0000

    Update gcloud_bitbucket_clone.sh

--- index_utils/gitNotes/gcloud_bitbucket_clone.sh
+++ index_utils/gitNotes/gcloud_bitbucket_clone.sh
@@ -1,7 +1,16 @@
 
-
 git clone https://bitbucket.org/indexproduction_SK/indexproduction.git
+
+git clone https://skydesmo@bitbucket.org/skydesmo/dataeng.git
+
+
+#git checkout -- path/to/file/to/revert
+
+cd indexproduction
+git pull origin
+
 paul.desmond@skytra.com
 HL1
 
-git clone https://skydesmo@bitbucket.org/skydesmo/dataeng.git
+cd ../dataeng
+git pull origin
