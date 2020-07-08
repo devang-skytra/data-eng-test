41ede8b2f7c337d5114c1460dc6f58dd7c4a8d10
Paul Desmond <skydesmo@bitbucket.org>
2020-07-08 21:40:39 +0100

    gcpsdk info

--- index_utils/bq/CALL_scratch_PaD_US.sp_pivot.sql
+++ index_utils/bq/CALL_scratch_PaD_US.sp_pivot.sql
@@ -1,10 +1,16 @@
-CALL scratch_PaD_US.sp_pivot(
-  'scratch_PaD_US.vw_hoffa'
-  , 'scratch_PaD_US.delete_pivotted' # destination table
-  , ['MoteName', 'TIMESTAMP_TRUNC(Timestamp, HOUR) AS hour'] # row_ids
-  , 'SensorName' # pivot_col_name
-  , 'Data' # pivot_col_value
-  , 8 # max_columns
-  , 'AVG' # aggregation
-  , 'LIMIT 10' # optional_limit
+
+
+create or replace view products_m.vw_P002 as 
+
+
+
+CALL products_m.sp_pivot(
+    'products_m.vw_P002'  		# source object
+  , 'products_m.P002_pivotted'  # destination table
+  , ['dt_of_issue']       		# row_ids
+  , 'unpiv_key'            		# pivot_col_name
+  , 'unpiv_val'          		# pivot_col_value
+  , 1000                        # column limit
+  , 'AVG'                       # aggregation
+  , 'LIMIT 1000000'             # row limit
 );
